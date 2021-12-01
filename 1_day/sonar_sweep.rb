eab5857e2e80a75e6411b4eed3be262dd1b0d9b4
Phillip Strom <phillip.strom@gmail.com>
2021-12-01 10:39:43 -0700

    Solve part 2

--- 1_day/sonar_sweep.rb
+++ 1_day/sonar_sweep.rb
@@ -6,11 +6,19 @@ class SonarSweep
   end
 
   def count_depth_increases
+    # part 1
     depths.each_cons(2).count do |depth_1, depth_2|
       depth_1 < depth_2
     end
   end
+
+  def count_depth_windows
+    # part 2
+    depths.each_cons(3).map(&:sum).each_cons(2).count do |window_1, window_2|
+      window_1 < window_2
+    end
+  end
 end
 
-
 puts SonarSweep.new('input.txt').count_depth_increases
+puts SonarSweep.new('input.txt').count_depth_windows
--- 1_day/sonar_sweep_test.rb
+++ 1_day/sonar_sweep_test.rb
@@ -8,9 +8,14 @@ class SonarSweepTest < Minitest::Test
     assert_equal 10, sonar.depths.size
     assert sonar.depths.first.integer?
   end
-  
+
   def test_it_can_count_depth_increases
     sonar = SonarSweep.new('test_input.txt')
     assert_equal 7, sonar.count_depth_increases
+  end
+
+  def test_it_can_count_depth_windows
+    sonar = SonarSweep.new('test_input.txt')
+    assert_equal 5, sonar.count_depth_windows
   end
 end
