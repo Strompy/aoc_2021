420dddcc678c4d74c7b9e14d67763b1d05d370df
Phillip Strom <phillip.strom@gmail.com>
2021-12-01 10:27:17 -0700

    Solve part 1

--- 1_day/sonar_sweep.rb
+++ 1_day/sonar_sweep.rb
@@ -1,10 +1,16 @@
 class SonarSweep
   attr_reader :depths
+
   def initialize(input)
     @depths = IO.readlines(input).map(&:to_i)
   end
 
-  def increased_depth?(first_depth, second_depth)
-    first_depth < second_depth
+  def count_depth_increases
+    depths.each_cons(2).count do |depth_1, depth_2|
+      depth_1 < depth_2
+    end
   end
 end
+
+
+puts SonarSweep.new('input.txt').count_depth_increases
--- 1_day/sonar_sweep_test.rb
+++ 1_day/sonar_sweep_test.rb
@@ -8,14 +8,9 @@ class SonarSweepTest < Minitest::Test
     assert_equal 10, sonar.depths.size
     assert sonar.depths.first.integer?
   end
-
-  def test_it_can_increased_depth?
-    sonar = SonarSweep.new('input.txt')
-    assert_equal true, sonar.increased_depth?(200, 208)
-    assert_equal false, sonar.increased_depth?(210, 200)
-  end
-
+  
   def test_it_can_count_depth_increases
-
+    sonar = SonarSweep.new('test_input.txt')
+    assert_equal 7, sonar.count_depth_increases
   end
 end
