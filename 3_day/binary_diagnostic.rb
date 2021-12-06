f9876da218c317b91adb76b6f2d9d6a33958cdf6
Phillip Strom <phillip.strom@gmail.com>
2021-12-06 11:52:09 -0700

    Solve part 1

--- 3_day/binary_diagnostic.rb
+++ 3_day/binary_diagnostic.rb
@@ -1,9 +1,49 @@
 class BinaryDiagnostic
-  attr_reader :numbers
+  attr_reader :numbers, :gamma,  :epsilon
 
   def initialize(input)
-    @numbers = IO.readlines(input)
+    @numbers = IO.readlines(input).map(&:chomp)
+    @gamma, @epsilon = '', ''
+  end
+# part 1
+  def find_rates
+    numbers.first.size.times do |i|
+      zeros, ones = 0, 0
+      numbers.each do |n|
+        n[i] == '0' ? zeros += 1 : ones += 1
+      end
+      @gamma << (zeros > ones ? '0' : '1')
+      @epsilon << (zeros > ones ? '1' : '0')
+    end
   end
 
-  
+  def solve
+    @gamma.to_i(2) * @epsilon.to_i(2)
+  end
+
+  # def find_most_common_bit(index)
+  #   zeros, ones = 0, 0
+  #   numbers.each do |n|
+  #     n[index] == '0' ? zeros += 1 : ones += 1
+  #   end
+  #   zeros > ones ? '0' : '1'
+  # end
+  #
+  # def calculate_gamma_rate
+  #   numbers.first.size.times do |i|
+  #     @gamma << find_most_common_bit(i)
+  #   end
+  # end
+  #
+  # def calculate_epsilon_rate
+  #   flip = {'1' => '0', '0' => '1'}
+  #   numbers.first.size.times do |i|
+  #     @epsilon << flip[find_most_common_bit(i)]
+  #   end
+  # end
+
 end
+
+bd = BinaryDiagnostic.new('input.txt')
+bd.find_rates
+puts bd.solve
--- 3_day/binary_diagnostic_test.rb
+++ 3_day/binary_diagnostic_test.rb
@@ -11,15 +11,30 @@ class BinaryDiagnosticTest < MiniTest::Test
     assert_equal 12, @bd.numbers.size
   end
 
-  def test_it_calculate_gamma_rate
+  def test_it_can_find_most_common_bit
+    assert_equal '1', @bd.find_most_common_bit(0)
+  end
 
+  def test_it_calculate_gamma_rate
+    @bd.calculate_gamma_rate
+    assert_equal '10110', @bd.gamma
   end
 
   def test_it_calculate_epsilon_rate
-
+    @bd.calculate_epsilon_rate
+    assert_equal '01001', @bd.epsilon
   end
 
   def test_it_can_solve
+    @bd.calculate_gamma_rate
+    @bd.calculate_epsilon_rate
+    assert_equal 198, @bd.solve
+  end
 
+  def test_it_can_find_rates
+    @bd.find_rates
+    assert_equal '10110', @bd.gamma
+    assert_equal '01001', @bd.epsilon
+    assert_equal 198, @bd.solve
   end
 end
