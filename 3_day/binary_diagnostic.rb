5c00008e47545c647c12759e8029b611c30c5ea4
Phillip Strom <phillip.strom@gmail.com>
2021-12-06 14:00:47 -0700

    Solve part 2

--- 3_day/binary_diagnostic.rb
+++ 3_day/binary_diagnostic.rb
@@ -5,7 +5,7 @@ class BinaryDiagnostic
     @numbers = IO.readlines(input).map(&:chomp)
     @gamma, @epsilon = '', ''
   end
-# part 1
+  # part 1
   def find_rates
     numbers.first.size.times do |i|
       zeros, ones = 0, 0
@@ -21,13 +21,53 @@ class BinaryDiagnostic
     @gamma.to_i(2) * @epsilon.to_i(2)
   end
 
-  # def find_most_common_bit(index)
-  #   zeros, ones = 0, 0
-  #   numbers.each do |n|
-  #     n[index] == '0' ? zeros += 1 : ones += 1
-  #   end
-  #   zeros > ones ? '0' : '1'
-  # end
+  def oxy_rating
+    nums = numbers
+    index = 0
+    until nums.size == 1
+      bit = find_most_common_oxy_bit(nums, index)
+      nums = nums.select do |n|
+        n[index] == bit
+      end
+      index += 1
+    end
+    nums[0]
+  end
+
+  def co2_rating
+  # To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position.
+  # If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+  nums = numbers
+  index = 0
+  until nums.size == 1
+    bit = find_least_common_co2_bit(nums, index)
+    nums = nums.select do |n|
+      n[index] == bit
+    end
+    index += 1
+  end
+  nums[0]
+  end
+
+  def find_most_common_oxy_bit(nums, index)
+    zeros, ones = 0, 0
+    nums.each do |n|
+      n[index] == '0' ? zeros += 1 : ones += 1
+    end
+    ones >= zeros ? '1' : '0'
+  end
+
+  def find_least_common_co2_bit(nums, index)
+    zeros, ones = 0, 0
+    nums.each do |n|
+      n[index] == '0' ? zeros += 1 : ones += 1
+    end
+    ones >= zeros ? '0' : '1'
+  end
+
+  def solve_2
+    oxy_rating.to_i(2) * co2_rating.to_i(2)
+  end
   #
   # def calculate_gamma_rate
   #   numbers.first.size.times do |i|
--- 3_day/binary_diagnostic_test.rb
+++ 3_day/binary_diagnostic_test.rb
@@ -12,20 +12,24 @@ class BinaryDiagnosticTest < MiniTest::Test
   end
 
   def test_it_can_find_most_common_bit
+    skip
     assert_equal '1', @bd.find_most_common_bit(0)
   end
 
   def test_it_calculate_gamma_rate
+    skip
     @bd.calculate_gamma_rate
     assert_equal '10110', @bd.gamma
   end
 
   def test_it_calculate_epsilon_rate
+    skip
     @bd.calculate_epsilon_rate
     assert_equal '01001', @bd.epsilon
   end
 
   def test_it_can_solve
+    skip
     @bd.calculate_gamma_rate
     @bd.calculate_epsilon_rate
     assert_equal 198, @bd.solve
@@ -36,5 +40,17 @@ class BinaryDiagnosticTest < MiniTest::Test
     assert_equal '10110', @bd.gamma
     assert_equal '01001', @bd.epsilon
     assert_equal 198, @bd.solve
+  end
+
+  def test_it_can_find_oxy_rating
+    assert_equal '10111', @bd.oxy_rating
+  end
+
+  def test_it_can_find_co2_rating
+    assert_equal '01010', @bd.co2_rating
+  end
+
+  def test_it_can_solve_part_2
+    assert_equal 230, @bd.solve_2
   end
 end
