2f6d85741ecf26bef02ceecfece186483e95bc49
Phillip Strom <phillip.strom@gmail.com>
2021-12-13 13:34:29 -0700

    Solve Day 7

--- 7_day/whales.rb
+++ 7_day/whales.rb
@@ -1,3 +1,34 @@
 class Whales
+  attr_reader :crabs
 
+  def initialize(input)
+    @crabs = IO.read(input).split(',').map(&:to_i)
+  end
+  # Part 1
+  def calculate_fuel
+    median = calculate_median(@crabs.sort)
+    crabs.sum do |crab|
+      (crab - median).abs
+    end
+  end
+
+  def calculate_median(nums)
+    size = nums.size
+    ((nums[(size - 1) / 2] + nums[size / 2]) / 2)
+  end
+  #  Part 2
+  def calculate_increasing_fuel
+    mean = calculate_mean(@crabs.sort)
+    crabs.sum do |crab|
+      change = (crab - mean).abs
+      change * (change + 1) / 2
+    end
+  end
+
+  def calculate_mean(nums)
+    (nums.sum / nums.size).round
+  end
 end
+
+puts Whales.new('input.txt').calculate_fuel
+puts Whales.new('input.txt').calculate_increasing_fuel
--- 7_day/whales_test.rb
+++ 7_day/whales_test.rb
@@ -4,10 +4,15 @@ require './whales'
 
 class WhalesTest < Minitest::Test
   def setup
-    @whales = Whales.new('test_input.txt')
+    @whale = Whales.new('test_input.txt')
   end
 
   def test_it_can_read_lines
-    
+    assert_equal 10, @whale.crabs.size
+    assert_equal 16, @whale.crabs.first
+  end
+
+  def test_it_can_calculate_fuel
+    assert_equal 37, @whale.calculate_fuel
   end
 end
