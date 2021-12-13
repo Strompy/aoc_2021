1c3f4869236e427017003453189166890588b655
Phillip Strom <phillip.strom@gmail.com>
2021-12-13 10:27:08 -0700

    Solve part 2

--- 6_day/laternfish.rb
+++ 6_day/laternfish.rb
@@ -1,3 +1,27 @@
 class Laternfish
+  attr_reader :school
 
+  def initialize(input)
+    fishes = File.read(input)
+    @school = (0..8).map do |day|
+      fishes.count(day.to_s)
+    end
+  end
+
+  def simulate_days(days)
+    days.times do |day|
+      @school.rotate!
+      @school[6] += school[8]
+    end
+  end
+
+  def count_fish
+    @school.sum
+  end
 end
+
+s = Laternfish.new('input.txt')
+s.simulate_days(256)
+puts s.count_fish
+
+1,738,377,086,345
--- 6_day/laternfish_test.rb
+++ 6_day/laternfish_test.rb
@@ -8,6 +8,20 @@ class LaternfishTest < Minitest::Test
   end
 
   def test_it_can_read_lines
+    assert_equal 9, @fish.school.size
+    assert_equal 2, @fish.school[3]
+  end
 
+  def test_it_can_simulate_days
+    @fish.simulate_days(18)
+    assert_equal 26, @fish.count_fish
+
+    @fish = Laternfish.new('test_input.txt')
+    @fish.simulate_days(80)
+    assert_equal 5934, @fish.count_fish
+
+    @fish = Laternfish.new('test_input.txt')
+    @fish.simulate_days(256)
+    assert_equal 26984457539, @fish.count_fish
   end
 end
